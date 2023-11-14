7735ad6cbb0419566047c4a6c8fc0b1fa556850d
y1lmaz-ahmet <y.ahmet@outlook.be>
2023-11-14 12:57:45 +0100

    adding the EJS template engine

--- app.js
+++ app.js
@@ -4,42 +4,40 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); //EJS template engine
+
 app.use(express.static("public")); // accessing STATIC files
 app.use(express.urlencoded({ extended: false })); //parsing data
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 }); // index.html
 
-app.get("/confirm", function (request, response) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  response.sendFile(htmlFilePath);
+app.get("/confirm", function (request, res) {
+  res.render("confirm");
 });
 
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath);
+  res.render("restaurants");
 });
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath);
+  res.render("about");
 });
 
 app.get("/recommend", function (req, res) {
-  const htmlPathFile = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlPathFile);
+  res.render("recommend");
 });
 
 //POST
 app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-  storedRestaurants.push(restaurant);
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  const restaurant = req.body; //reqest the body
+  const filePath = path.join(__dirname, "data", "restaurants.json"); // configure the location of the json
+  const fileData = fs.readFileSync(filePath); // read the json file
+  const storedRestaurants = JSON.parse(fileData); // parse data into json
+  storedRestaurants.push(restaurant); // push the json in the json file
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants)); // update the json file with the newest json data
   res.redirect("/confirm");
 });
 
